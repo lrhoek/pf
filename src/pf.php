7ed0463d7a79e01ddae9e00880519652aec87767
Laurens Hoek <lrhoek@gmail.com>
2025-12-24 18:01:32 +0100

    Adding some array functions & cleanup

--- src/pf.php
+++ src/pf.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace Lrhoek\pf;
 
-use Closure;
 use Crell\fp;
-use Iterator;
-use LimitIterator;
 
 /**
  * Return callable for fp\iterate
@@ -19,31 +16,56 @@ function iterate(callable $mapper) : callable {
 /**
  * Same as fp\ittake, but with offset
  */
-function ittake(int $offset, int $count = 1) : Closure {
-    return static function (array|Iterator $a) use ($offset, $count) : iterable {
-        yield from is_array($a)
-            ? array_slice($a, $offset, $count)
-            : new LimitIterator($a, $offset, $count);
+function ittake(int $offset, int $count = 1) : callable {
+    return static function (array|\Iterator $a) use ($offset, $count) : iterable {
+        yield from \is_array($a)
+            ? \array_slice($a, $offset, $count)
+            : new \LimitIterator($a, $offset, $count);
     };
 }
 
 /**
- * Return callable for usort
+ * Return unary callable for array_any
+ */
+function array_any(callable $c) : callable {
+    return function (array $a) use ($c) : bool {
+        return \array_any($a, $c);
+    };
+}
+
+/**
+ * Return unary callable for array_all
+ */
+function array_all(callable $c) : callable {
+    return function (array $a) use ($c) : bool {
+        return \array_all($a, $c);
+    };
+}
+
+/**
+ * Return unary callable for array_chunk
+ */
+function array_chunk(int $length, bool $preserve_keys = false) : callable {
+    return function (array $a) use ($length, $preserve_keys) : array {
+        return \array_chunk($a, $length, $preserve_keys);
+    };
+}
+
+/**
+ * Return unary callable for array_slice
+ */
+function array_slice(int $offset, ?int $length = null, bool $preserve_keys = false) : callable {
+    return function (array $a) use ($offset, $length, $preserve_keys) : array {
+        return \array_slice($a, $offset, $length, $preserve_keys);
+    };
+}
+
+/**
+ * Return unary callable for usort
  */
 function usort(callable $c) : callable {
-    return function (array $a) use ($c) {
+    return function (array $a) use ($c) : array {
         \usort($a, $c);
         return $a;
     };
-}
-
-/**
- * Adds a number to the value at $array[ $key ]
- * Or sets it if it is null
- * @CURSED
- */
-function add(array $array, int|string $key, int $number) : array {
-    $array[$key] ??= 0;
-    $array[$key] += $number;
-    return $array;
 }
